383be579cdb22affa6177a4281d43edcf2458095
Batınay Ünsel <batinayunsel@gmail.com>
2021-10-11 02:29:38 +0300

    self study added

---
+++ self.c
@@ -0,0 +1,30 @@
+#include<stdio.h>
+//Batınay Ünsel
+int cubed(int a){
+    return a*a*a; //this a is return to n^3.
+
+}
+
+
+void main(){
+    int a = 3;
+    int result;
+
+    result = cubed(a);
+    printf("The %d cubed is : %d\n", a, result);
+
+    printf("***********\n");
+    printf("*           *\n");
+    printf("*             *\n");
+    printf("*              *\n");
+    printf("*             *\n");
+    printf("*            *\n");
+    printf("************\n");
+    printf("*            *\n");
+    printf("*              *\n");
+    printf("*              *\n");
+    printf("*             *\n");
+    printf("*            *\n");
+    printf("************\n");
+    
+}
